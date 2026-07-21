8ce135e327b4f53d87f627c5a2185254c28c14f3
Aurora75 <mirkovic.uk@gmail.com>
2026-07-21 14:56:54 +0100

    Migrate test/id.test.ts (task 5597fc26-31cd-4302-bb1a-5ca52544c9eb)

---
+++ test/id.test.ts
@@ -0,0 +1,17 @@
+import { describe, it, expect } from "vitest";
+import { newId, newShortRef } from "../../src/lib/id";
+
+describe("id", () => {
+  it("newId returns a non-empty unique string", () => {
+    const a = newId();
+    const b = newId();
+    expect(typeof a).toBe("string");
+    expect(a.length).toBeGreaterThan(0);
+    expect(a).not.toBe(b);
+  });
+
+  it("newShortRef returns a T- prefixed uppercase ref", () => {
+    const ref = newShortRef();
+    expect(ref).toMatch(/^T-[A-Z2-9]{6}$/);
+  });
+});
